1e20149ceb6b40786191e1274e03303f6e646c88
LeelooDallas <barbeylise@gmail.com>
2025-12-21 14:48:58 +0100

    feat(faq): section faq

--- src/components/atoms/Paragraph/Paragraph.types.ts
+++ src/components/atoms/Paragraph/Paragraph.types.ts
@@ -3,4 +3,5 @@ export interface ParagraphProps {
   size?: 'sm' | 'base' | 'lg';
   variant?: 'blue' | 'red' | 'gold' | 'white' | 'black';
   align?: 'left' | 'center' | 'right';
+  spacing?: 'none' | 'xs' | 'sm' | 'base' | 'lg';
 }
